3fc593ce2a43501e27a6684b8defb10e2a575216
FranciscoAsBer <franciscoascenciober@gmail.com>
2023-08-21 22:30:05 -0600

    esto es una prueba

--- holamundo/Form1.Designer.cs
+++ holamundo/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(11, 125);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(261, 13);
+            this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "holamundo KASNDAMS;LDMASLD;ALSMDcagadero";
+            this.label1.Text = "holamundo";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
--- holamundo/Form1.cs
+++ holamundo/Form1.cs
@@ -19,7 +19,7 @@ namespace holamundo
 
         private void label1_Click ( object sender, EventArgs e )
         {
-            //hola mundo cagadero MIERDA
+            //hola mundo cagadero MIERDA deja de joder
 
         }
     }
